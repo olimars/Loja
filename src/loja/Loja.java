05fe11f3c89beadef0186c666de0dc353267e2b7
Gmarques <gm.marques2010@gmail.com>
2018-10-09 10:44:19 +0100

    void

--- src/loja/Loja.java
+++ src/loja/Loja.java
@@ -14,7 +14,7 @@ public class Loja {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static voi main(String[] args) {
         // TODO 
     }
     
