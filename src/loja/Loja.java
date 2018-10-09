aa0744dc7b367950901fe3154822058a70189f6d
Gmarques <gm.marques2010@gmail.com>
2018-10-09 10:46:05 +0100

    void alterado

--- src/loja/Loja.java
+++ src/loja/Loja.java
@@ -14,7 +14,7 @@ public class Loja {
     /**
      * @param args the command line arguments
      */
-    public static voi main(String[] args) {
+    public static void main(String[] args) {
         // TODO 
     }
     
